9bac5bdd607943cd26973ed5d17a136a5fd2c1b3
EL MEHDI NJA <mrmy0wnbus1n3ss@hotmail.com>
2023-08-09 22:20:00 +0100

    cpp

--- Cpp-Module_00/ex01/main.cpp
+++ Cpp-Module_00/ex01/main.cpp
@@ -3,20 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enja <enja@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ky05h1n <ky05h1n@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 18:03:07 by ky05h1n           #+#    #+#             */
-/*   Updated: 2023/08/04 20:29:43 by enja             ###   ########.fr       */
+/*   Updated: 2023/08/07 17:11:12 by ky05h1n          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
-
 int main()
 {
     PhoneBook contactobj;
+    int signal = 0;
     int i = 0;
+    int j = 0;
 
     std::string option;
     std::cout << "\033[2J\033[H";
@@ -31,17 +32,23 @@ int main()
             contactobj.addinfo(i);
             i++;
             if (i == 8)
+            {
                 i = 0;
+                j = 8;
+            }
+            if (j != 8)
+                j++;
+            signal = 1;
         }
         else if (option == "SEARCH")
         {
-            if (i >= 1)
+            if (signal == 1)
             {
                 std::cout << "\033[2J\033[H";
                 start();
                 std::cout << "|\033[1;36m   index    \033[0m|\033[1;36m first name \033[0m|\033[1;36m last name  \033[0m|\033[1;36m nick name  \033[0m|" << std::endl;
                 std::cout << "-----------------------------------------------------" << std::endl;
-                for (int n = 0 ; n < i; n++)
+                for (int n = 0; n < j; n++)
                     contactobj.showinfo(n);
                 std::cout << "\n\n";
             }
@@ -50,10 +57,9 @@ int main()
                 std::cout << "\033[2J\033[H";
                 start();
                 std::cout << "\033[1;31mWarning : \033[0m";
-                std::cout << "there is no contact in the data base" << std::endl << std::endl;
+                std::cout << "there is no contact in the data base" << std::endl
+                          << std::endl;
             }
-                    
-                
         }
         else if (option == "EXIT")
             return (0);
