a709dec4101aabba58e6a601989c557c45a980f9
El mehdi Nja <enja@e1r3p10.1337.ma>
2023-08-14 03:46:12 +0100

    cpp

--- Cpp-Module_01/ex04/main.cpp
+++ Cpp-Module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: enja <enja@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 08:01:30 by enja              #+#    #+#             */
-/*   Updated: 2023/08/13 08:23:15 by enja             ###   ########.fr       */
+/*   Updated: 2023/08/14 03:42:17 by enja             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 #include <fstream>
 
 int main(int ac , char **av)
-{
-    if (ac != 4)
+{   
+    int lastpos = 0;
+    if (ac != 4 || !av[2][0] || !av[3][0])
     {
         std::cout << "Usage: " << av[0] << " <filename> <s1> <s2>" << std::endl;
         return 1;
@@ -23,12 +24,28 @@ int main(int ac , char **av)
 
     std::ifstream file(av[1], std::ios::in | std::ios::out);
     if(!file)
-        std::cout << "error no file" << std::endl;
-
-        
-
-    
-    
-
+    {
+            std::cout << "error no file" << std::endl;
+            return 1;
+    }
+            std::ofstream outputfile("newfile.replace");
+            std::string line;
+            int pos = 0;
+            while(getline(file, line))
+            {
+                    while (1)
+                    {
+                            pos = line.find(av[2],lastpos);
+                             if (pos == -1)
+                                break;
+                            line.erase(pos, strlen(av[2]));
+                            line.insert(pos,av[3],strlen(av[3]));
+                            lastpos = pos + strlen(av[3]);
+                    }
+                    std::ofstream appendfile("newfile.replace", std::ios::app);
+                    appendfile << line << std::endl;
+                    lastpos = 0;
+            }
+                    
     return 0;
 }
