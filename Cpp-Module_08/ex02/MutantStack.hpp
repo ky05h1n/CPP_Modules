75abbdcf19de20e110a2db3a9b71536d67182bc9
El mehdi Nja <enja@c3r1p10.1337.ma>
2023-10-03 02:48:39 +0100

    cpp

---
+++ Cpp-Module_08/ex02/MutantStack.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enja <enja@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 11:25:50 by enja              #+#    #+#             */
+/*   Updated: 2023/10/02 12:02:26 by enja             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <stack>
+
+template<typename T>
+class MutantStack : public std::stack{
+
+
+            public:
+
+                    iterator begin();
+                    iterator end();
+            
+            
+            class iterator{
+                
+            }
+};
