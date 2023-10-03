97072b32c38c5c182d6ff3c8d3b0a53584948681
El mehdi Nja <enja@c3r1p10.1337.ma>
2023-10-03 10:03:38 +0100

    cpp

--- Cpp-Module_08/ex02/MutantStack.hpp
+++ Cpp-Module_08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: enja <enja@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 11:25:50 by enja              #+#    #+#             */
-/*   Updated: 2023/10/02 12:02:26 by enja             ###   ########.fr       */
+/*   Updated: 2023/10/03 09:44:40 by enja             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,45 @@
 
 #include <iostream>
 #include <stack>
+#include <list>
+#include <algorithm>
 
 template<typename T>
-class MutantStack : public std::stack{
+class MutantStack : public std::stack<T>{
 
 
-            public:
+			public:
+					
+					typedef typename std::stack<T>::container_type::iterator iterator;
+					typedef typename std::stack<T>::container_type::reverse_iterator reviter;
 
-                    iterator begin();
-                    iterator end();
-            
-            
-            class iterator{
-                
-            }
+					MutantStack() : std::stack<T>(){}
+					~MutantStack(){}
+					MutantStack(const MutantStack& obj)
+					{
+						*this = obj;
+					}
+					const MutantStack& operator = (const MutantStack& obj)
+					{
+						this->c = obj.c;
+						return *this;
+					}
+
+
+                    iterator begin()
+					{
+						return iterator(this->c.begin());
+					}
+                    iterator end()
+					{
+						return iterator(this->c.end());
+					}
+					reviter revbegin()
+					{
+						return reviter(this->c.rbegin());
+					}
+					reviter revend()
+					{
+						return reviter(this->c.rend());
+					}
 };
--- Cpp-Module_08/ex02/main.cpp
+++ Cpp-Module_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: enja <enja@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 11:25:52 by enja              #+#    #+#             */
-/*   Updated: 2023/10/02 11:53:17 by enja             ###   ########.fr       */
+/*   Updated: 2023/10/03 10:00:37 by enja             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,89 @@
 
 int main()
 {
-      
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+    return 0;
 }
+
+// int main()
+// {
+//     MutantStack<int> obj1;
+//     MutantStack<int> obj2;
+
+//     for (int i = 0; i <= 5; i++)
+//     {
+//         obj1.push(i);
+//     }
+    
+//     MutantStack<int>::iterator it = obj1.begin();
+    
+//     for (int i = 0; i <= 5; i ++)
+//     {
+//         std::cout << *it++ << std::endl;
+//     }
+
+//     obj2 = obj1;
+//     sort(obj2.begin(), obj2.end(), std::greater<int>());
+//     it = obj2.begin();
+//     std::cout << "---=========================---" << std::endl;
+//     for (int i = 0; i <= 5; i ++)
+//     {
+//         std::cout << *it++ << std::endl;
+//     }
+//     std::cout << "---=========================---" << std::endl;
+//     std::cout << "fist stack : " << obj1.top() << " second stack : " <<  obj2.top() << std::endl;
+//     for (int i = 0; i <= 4; i++)
+//     {
+//         obj1.pop();
+//         obj2.pop();
+//     }
+//     std::cout << "---=========================---" << std::endl;
+//     std::cout << "fist stack : " << obj1.top() << " second stack : " <<  obj2.top() << std::endl;
+// }
+
+
+// int main()
+// {
+//     std::list<int> mstack;
+//     mstack.push_front(5);
+//     mstack.push_front(17);
+//     std::cout << *mstack.begin() << std::endl;
+//     mstack.pop_front();
+//     std::cout << mstack.size() << std::endl;
+//     mstack.push_back(3);
+//     mstack.push_back(5);
+//     mstack.push_back(737);
+//     mstack.push_back(0);
+
+//     std::list<int>::iterator it = mstack.begin();
+//     std::list<int>::iterator ite = mstack.end();
+//     ++it;
+//     --it;
+//     while (it != ite)
+//     {
+//         std::cout << *it << std::endl;
+//         ++it;
+//     }
+//     std::list<int> s(mstack);
+//     return 0;
+// }
